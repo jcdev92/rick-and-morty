22b7d58b927f085666d385360e71ddaa715c7901
jcdev92 <jcdeveloper92@gmail.com>
2023-05-14 01:59:24 -0400

    styles added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = ({ setSearchInfo }) => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const input = e.target.search.value;
+    if (input === "") {
+      alert("Please enter a location ID or Name");
+      return;
+    }
     setSearchInfo(input);
+    setInputValue("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="search">
       <input
         id="search"
         type="text"
         placeholder="Search by location ID or Name"
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
       />
-      <button type="submit">Search</button>
+      <button type="submit" className="search-btn">
+        Search
+      </button>
     </form>
   );
 };
